565e23c071b25090845da1c8123180adf27260db
Isilin <casa2pir@hotmail.fr>
2017-11-23 22:32:28 +0100

    Fix merge (ter)

--- src/utility/debug/exception.cpp
+++ src/utility/debug/exception.cpp
@@ -36,76 +36,76 @@
 
 */
 
-#include "Debug\Exception.hpp"
+#include "utility/debug/exception.hpp"
 
 namespace ece
 {
 	/* ########## FileException methods ########## */
 
-	FileException FileException::makeException(const FileCodeError codeError, const std::string & filename)
+	FileException::FileException(const FileCodeError codeError, const std::string & filename) : Exception()
 	{
 		switch (codeError) {
 		case BAD_PATH:
-			return Exception<FileException>::makeException("Code %. This file doesn't exist: %.", (int)codeError, filename);
+			this->setMessage("Code %. This file doesn't exist: %.", (int)codeError, filename);
 			break;
 		case PARSE_ERROR:
-			return Exception<FileException>::makeException("Code %. Error while trying to parse %. Check the content format.", (int)codeError, filename);
+			this->setMessage("Code %. Error while trying to parse %. Check the content format.", (int)codeError, filename);
 			break;
 		default:
-			return Exception<FileException>::makeException("Code %. Undefined error on file %.", (int)codeError, filename);
+			this->setMessage("Code %i. Undefined error on file %.", (int)codeError, filename);
 			break;
 		}
 	}
 
 	/* ########## BadInputException methods ########## */
 
-	BadInputException BadInputException::makeException(const std::string & details)
+	BadInputException::BadInputException(const std::string & details)
 	{
-		return Exception<BadInputException>::makeException("Bad input: %.", details);
+		this->setMessage("Bad input: %.", details);
 	}
 
 	/* ########## InitializationException methods ########## */
 
-	InitializationException InitializationException::makeException(const std::string & target)
+	InitializationException::InitializationException(const std::string & target)
 	{
-		return Exception<InitializationException>::makeException("% has failed to initialize.", target);
+		this->setMessage("% has failed to initialize.", target);
 	}
 
 	/* ########## MemoryAccessException methods ########## */
 
-	MemoryAccessException MemoryAccessException::makeException(const std::string & target)
+	MemoryAccessException::MemoryAccessException(const std::string & target)
 	{
-		return Exception<MemoryAccessException>::makeException("Bad access to %. The pointer has expired.", target);
+		this->setMessage("Bad access to %. The pointer has expired.", target);
 	}
 
-	MemoryAccessException MemoryAccessException::makeException(const std::string & target, const std::string & origin)
+	MemoryAccessException::MemoryAccessException(const std::string & target, const std::string & origin)
 	{
-		return Exception<MemoryAccessException>::makeException("Bad access to % from %. The pointer has expired.", target, origin);
+		this->setMessage("Bad access to % from %. The pointer has expired.", target, origin);
 	}
 
 	/* ########## OutOfRangeException methods ########## */
 
-	OutOfRangeException OutOfRangeException::makeException(const std::string & type)
+	OutOfRangeException::OutOfRangeException(const std::string & type)
 	{
-		return Exception<OutOfRangeException>::makeException("Out of range access for %.", type);
+		this->setMessage("Out of range access for %.", type);
 	}
 
-	OutOfRangeException OutOfRangeException::makeException(const std::string & type, const int id)
+	OutOfRangeException::OutOfRangeException(const std::string & type, const int id)
 	{
-		return Exception<OutOfRangeException>::makeException("Out of range access for % %.", type, id);
+		this->setMessage("Out of range access for % %.", type, id);
 	}
 
 	/* ########## ResourceException methods ########## */
 
-	ResourceException ResourceException::makeException(const std::string & target, const unsigned short int id)
+	ResourceException::ResourceException(const std::string & target, const unsigned short int id)
 	{
-		return Exception<ResourceException>::makeException("The resource % with the ID % is not available.", target, id);
+		this->setMessage("The resource % with the ID % is not available.", target, id);
 	}
 
 	/* ########## DivideByZeroException methods ########## */
 
-	DivideByZeroException DivideByZeroException::makeException(const std::string & origin)
+	DivideByZeroException::DivideByZeroException(const std::string & origin)
 	{
-		return Exception<DivideByZeroException>::makeException("A division by zero has been handle in %", origin);
+		this->setMessage("A division by zero has been handle in %", origin);
 	}
 }
