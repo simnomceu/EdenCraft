ec97bd263ac7f12354969325a189d4b98b65e775
Isilin <casa2pir@hotmail.fr>
2017-12-19 14:13:26 +0100

    Complete documentation for EnumTags.

--- include/utility/enum/enum_flags.hpp
+++ include/utility/enum/enum_flags.hpp
@@ -6,16 +6,16 @@
 namespace ece
 {
 /**
- * @define ENUM_FLAGS_OPERATOR
- * @brief
+ * @def ENUM_FLAGS_OPERATOR(NAME, OP)
+ * @brief Defines a specific overloaded bit to bit operator for a specific class.
  */
 #define ENUM_FLAGS_OPERATOR(NAME, OP) \
 	inline constexpr NAME operator OP(NAME leftHandSide, NAME rightHandSide) noexcept \
 	{ return static_cast<NAME>(static_cast<std::underlying_type_t<NAME>>(leftHandSide) OP static_cast<std::underlying_type_t<NAME>>(rightHandSide)); }
 
 /**
- * @define ENUM_FLAGS_OPERATOR_OVERLOAD
- * @brief
+ * @def ENUM_FLAGS_OPERATOR_OVERLOAD(NAME)
+ * @brief Defines all bit to bit operators for a specific class.
  */
 #define ENUM_FLAGS_OPERATOR_OVERLOAD(NAME) \
 	inline constexpr NAME operator ~(NAME value) noexcept \
@@ -25,8 +25,9 @@ namespace ece
 	ENUM_FLAGS_OPERATOR(NAME, ^)
 
 /**
- * @define EnumFlagsT
- * @brief
+ * @define EnumFlagsT(TYPE, NAME)
+ * @brief Defines a C++14 enumeration to be used has C-like tags.
+ * Thus, things like MyEnum::A | MyEnum::B make sense now.
  */
 #define EnumFlagsT(TYPE, NAME) \
 	enum class NAME; \
@@ -34,8 +35,8 @@ namespace ece
 	enum class NAME : TYPE
 
 /**
- * @define EnumFlags
- * @brief
+ * @define EnumFlags(NAME)
+ * @brief Defines a C++14 enumeration to be used has C-like tags, with default type as char.
  */
 #define EnumFlags(NAME) \
 	EnumFlagsT(char, NAME)
