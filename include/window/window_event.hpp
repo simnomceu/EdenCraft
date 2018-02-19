a12e5f73728d628a9435c9e0d3cb74f41ba849f5
Isilin <casa2pir@hotmail.fr>
2018-02-19 17:36:43 +0100

    [WIP] Documentation for Window::WindowEvent module.

--- include/window/window_event.hpp
+++ include/window/window_event.hpp
@@ -35,26 +35,12 @@
 
 */
 
+#ifndef WINDOW_EVENT_HPP
+#define WINDOW_EVENT_HPP
+
+#include "window/window_event/event_handler.hpp"
+#include "window/window_event/input_event.hpp"
+#include "window/window_event/keyboard.hpp"
 #include "window/window_event/mouse.hpp"
 
-namespace ece
-{
-	std::array<bool, 10> Mouse::_states;
-	IntVector2u Mouse::_position;
-
-	bool Mouse::isKeyPressed(const Button code)
-	{
-		if (code == Mouse::Button::ECE_MOUSE_NONE) {
-			throw std::runtime_error("That code is not a valid key.");
-		}
-		return Mouse::_states[static_cast<unsigned int>(code)];
-	}
-
-	void Mouse::pressKey(const Button code, const bool state)
-	{
-		if (code == Mouse::Button::ECE_MOUSE_NONE) {
-			throw std::runtime_error("That code is not a valid key.");
-		}
-		Mouse::_states[static_cast<unsigned int>(code)] = state;
-	}
-}
+#endif // WINDOW_EVENT_HPP
