42c85dbbaf71e3216279ae6700cbee4c2b6a5dc0
Isilin <casa2pir@hotmail.fr>
2018-08-16 14:12:43 +0200

    /bubble_volley: /bubble_volley:

--- src/renderer/common/render_window.cpp
+++ src/renderer/common/render_window.cpp
@@ -67,19 +67,24 @@ namespace ece
 
 			void RenderWindow::open()
 			{
-				Window::open();
-				try {
-					this->_context->create(*this);
-				}
-				catch (Exception & /*e*/) {
-					throw;
-				}
-				catch (std::runtime_error & e) {
-					ServiceLoggerLocator::getService().logError(e.what());
-				}
+				if (!this->isOpened()) {
+					this->_adapter->createWindow();
+					this->_isOpened = true;
 
-				this->_currentViewport.resetViewport(Rectangle<float>(0.0f, 0.0f, static_cast<float>(this->getSize()[0]), static_cast<float>(this->getSize()[1])));
-				this->_viewportHasChanged = true;
+					try {
+						this->_context->create(*this);
+					}
+					catch (Exception & /*e*/) {
+						throw;
+					}
+					catch (std::runtime_error & e) {
+						ServiceLoggerLocator::getService().logError(e.what());
+					}
+
+					this->_currentViewport.resetViewport(Rectangle<float>(0.0f, 0.0f, static_cast<float>(this->getSize()[0]), static_cast<float>(this->getSize()[1])));
+					this->_viewportHasChanged = true;
+					this->onWindowOpened();
+				}
 			}
 
 			IntVector2u RenderWindow::getSize() const
