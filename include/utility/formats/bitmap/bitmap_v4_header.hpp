a77c88fc6424dfb9dc1a3aea81d13790e0860747
Isilin <casa2pir@hotmail.fr>
2019-03-04 22:10:00 +0100

    [Add] bitmap headers.

---
+++ include/utility/formats/bitmap/bitmap_v4_header.hpp
@@ -0,0 +1,89 @@
+/*
+
+	oooooooooooo       .o8                          .oooooo.                       .o88o.     .
+	`888'     `8      "888                         d8P'  `Y8b                      888 `"   .o8
+	 888          .oooo888   .ooooo.  ooo. .oo.   888          oooo d8b  .oooo.   o888oo  .o888oo
+	 888oooo8    d88' `888  d88' `88b `888P"Y88b  888          `888""8P `P  )88b   888      888
+	 888    "    888   888  888ooo888  888   888  888           888      .oP"888   888      888
+	 888       o 888   888  888    .o  888   888  `88b    ooo   888     d8(  888   888      888 .
+	o888ooooood8 `Y8bod88P" `Y8bod8P' o888o o888o  `Y8bood8P'  d888b    `Y888""8o o888o     "888"
+
+															ooooo     ooo     .    o8o  oooo   o8o      .
+															`888'     `8'   .o8    `"'  `888   `"'    .o8
+															 888       8  .o888oo oooo   888  oooo  .o888oo oooo    ooo
+															 888       8    888   `888   888  `888    888    `88.  .8'
+															 888       8    888    888   888   888    888     `88..8'
+															 `88.    .8'    888 .  888   888   888    888 .    `888'
+															   `YbodP'      "888" o888o o888o o888o   "888"     .8'
+																											.o..P'
+																											`Y8P'
+
+				This file is part of EdenCraft Engine - Utility module.
+				Copyright(C) 2018 Pierre Casati (@IsilinBN)
+
+				This program is free software : you can redistribute it and/or modify
+				it under the terms of the GNU General Public License as published by
+				the Free Software Foundation, either version 3 of the License, or
+				(at your option) any later version.
+
+				This program is distributed in the hope that it will be useful,
+				but WITHOUT ANY WARRANTY; without even the implied warranty of
+				MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+				GNU General Public License for more details.
+
+				You should have received a copy of the GNU General Public License
+				along with this program.If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef BITMAP_V4_HEADER_HPP
+#define BITMAP_V4_HEADER_HPP
+
+#include "utility/config.hpp"
+#include "utility/pch.hpp"
+
+namespace ece
+{
+	namespace utility
+	{
+		namespace formats
+		{
+			namespace bitmap
+			{
+				struct BitmapV4Header
+				{
+					std::uint32_t size;
+					std::uint32_t width;
+					std::uint32_t height;
+					std::uint16_t planes;
+					std::uint16_t bpp;
+					std::uint32_t compression;
+					std::uint32_t imagesize;
+					std::uint32_t xResolution;
+					std::uint32_t yResolution;
+					std::uint32_t numberOfColorsUsed;
+					std::uint32_t numberOfImportantColors;
+					std::uint32_t redMask;
+					std::uint32_t greenMask;
+					std::uint32_t blueMask;
+					std::uint32_t alphaMask;
+					std::uint32_t colorSpaceType;
+					std::uint32_t xRedEndpoint;
+					std::uint32_t yRedEndpoint;
+					std::uint32_t yRedEndpoint;
+					std::uint32_t xGreenEndpoint;
+					std::uint32_t yGreenEndpoint;
+					std::uint32_t zGreenEndpoint;
+					std::uint32_t xBlueEndpoint;
+					std::uint32_t yBlueEndpoint;
+					std::uint32_t zBlueEndpoint;
+					std::uint32_t gammaRed;
+					std::uint32_t gammaGreen;
+					std::uint32_t gammaBlue;
+				};
+			} // namespace bitmap
+		} // namespace formats
+	} // namespace utility
+} // namespace ece
+
+#endif // BITMAP_V4_HEADER_HPP
