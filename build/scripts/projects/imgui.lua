f6e331b0fa86d0b0ed721375b243961511cc47ce
Isilin <casa2pir@hotmail.fr>
2019-05-23 18:14:28 +0200

    [Add] Dll api configuration for imgui.

--- build/scripts/projects/imgui.lua
+++ build/scripts/projects/imgui.lua
@@ -8,7 +8,7 @@ settings:setName("imgui")
 settings:setType("Lib")
 settings:addAdditionalSources { "../extlibs/imgui/*.cpp" }
 settings:addAdditionalHeaders { "../extlibs/imgui/*.h" }
-settings:addPreprocessors("Common", { "IMGUI_DISABLE_OBSOLETE_FUNCTIONS" })
+settings:addPreprocessors("Common", { "IMGUI_DISABLE_OBSOLETE_FUNCTIONS", "IMGUI_USER_CONFIG=\"imgui/imgui_config.hpp\"" })
 settings:enablePCH(false)
 
 return settings
